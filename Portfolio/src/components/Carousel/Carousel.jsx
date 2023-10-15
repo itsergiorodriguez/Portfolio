33039fdf57c1da9ed560b75f4c040d840dd7435f
itsergiorodriguez <it.sergiorodriguez@gmail.com>
2023-10-15 12:07:48 -0300

    se agrega img carousel

--- Portfolio/src/components/Carousel/Carousel.jsx
+++ Portfolio/src/components/Carousel/Carousel.jsx
@@ -6,6 +6,7 @@ import img2 from '../../assets/img/img1.png';
 import img3 from '../../assets/img/img2.png';
 import img4 from '../../assets/img/img3.png';
 import img5 from '../../assets/img/img4.png';
+import img6 from '../../assets/img/img5.png'
 import './carousel.css';
 
 
